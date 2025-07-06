542b6558b4ed849e3bbf3331556508a63cbce3ff
Aleh Kavalkou <osk08880@yandex.ru>
2025-07-06 03:33:20 +0300

    Добавлен конвертер в CSV файл.

---
+++ src/util/TaskConverterCsv.java
@@ -0,0 +1,35 @@
+package util;
+
+import model.Epic;
+import model.SubTask;
+import model.Task;
+
+public class TaskConverterCsv {
+
+    public static String toString(Task task) {
+        String[] fields = {
+                String.valueOf(task.getId()),
+                task.getType().name(),
+                task.getTitle(),
+                task.getStatus().name(),
+                task.getDetails(),
+                task instanceof SubTask ? String.valueOf(((SubTask) task).getEpicId()) : ""
+        };
+        return String.join(",", fields);
+    }
+
+    public static Task fromString(String line) {
+        String[] parts = line.split(",", -1); // -1 сохраняет пустые поля
+        int id = Integer.parseInt(parts[0]);
+        TaskType type = TaskType.valueOf(parts[1]);
+        String name = parts[2];
+        TaskStatus status = TaskStatus.valueOf(parts[3]);
+        String details = parts[4];
+
+        return switch (type) {
+            case TASK -> new Task(id, name, details, status);
+            case EPIC -> new Epic(id, name, details, status);
+            case SUBTASK -> new SubTask(id, name, details, status, Integer.parseInt(parts[5]));
+        };
+    }
+}
